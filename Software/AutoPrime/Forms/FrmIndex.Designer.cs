8e051e87fd376a7f375be4a7596c58e8de6e8022
Bruno Pavlović <bpavlovic20@foi.hr>
2022-12-30 19:07:51 +0100

    Preimenovana inicijalno kreirana Form1 u FrmIndex koja će biti početna forma naše aplikacije.

--- Software/AutoPrime/Forms/FrmIndex.Designer.cs
+++ Software/AutoPrime/Forms/FrmIndex.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AutoPrime
 {
-    partial class Form1
+    partial class FrmIndex
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmIndex
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "FrmIndex";
+            this.Text = "Početna";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
