3cec01b38fdc702499f0432d7b5d38d942235d95
Bruno Pavlović <bpavlovic20@foi.hr>
2022-12-30 19:06:11 +0100

    Kreirana forma za detaljni prikaz aukcija i oglasa.

---
+++ Software/AutoPrime/Forms/FrmDetailAdAndAuctionReview.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace AutoPrime.Forms
+{
+    partial class FrmDetailAdAndAuctionReview
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNazivOglasa = new System.Windows.Forms.TextBox();
+            this.Marka = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.pbSlika = new System.Windows.Forms.PictureBox();
+            this.btnPrethodna = new System.Windows.Forms.Button();
+            this.btnSljedeca = new System.Windows.Forms.Button();
+            this.checkBoxOstecenja = new System.Windows.Forms.CheckBox();
+            this.btnSlicni = new System.Windows.Forms.Button();
+            this.btnZanimljivi = new System.Windows.Forms.Button();
+            this.btnOstecenja = new System.Windows.Forms.Button();
+            this.btnKorime = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSlika)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNazivOglasa
+            // 
+            this.txtNazivOglasa.Location = new System.Drawing.Point(13, 13);
+            this.txtNazivOglasa.Name = "txtNazivOglasa";
+            this.txtNazivOglasa.Size = new System.Drawing.Size(100, 22);
+            this.txtNazivOglasa.TabIndex = 0;
+            // 
+            // Marka
+            // 
+            this.Marka.AutoSize = true;
+            this.Marka.Location = new System.Drawing.Point(19, 315);
+            this.Marka.Name = "Marka";
+            this.Marka.Size = new System.Drawing.Size(48, 16);
+            this.Marka.TabIndex = 1;
+            this.Marka.Text = "Marka:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 357);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Model: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 401);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Godina: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(2, 446);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Kilometraža: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 486);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Cijena: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(244, 690);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Lokacija vozila:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 531);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 16);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Motor: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(91, 309);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(91, 351);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(91, 395);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(91, 440);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(91, 480);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(91, 525);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 13;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(360, 687);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 14;
+            // 
+            // pbSlika
+            // 
+            this.pbSlika.Location = new System.Drawing.Point(183, 51);
+            this.pbSlika.Name = "pbSlika";
+            this.pbSlika.Size = new System.Drawing.Size(402, 170);
+            this.pbSlika.TabIndex = 15;
+            this.pbSlika.TabStop = false;
+            // 
+            // btnPrethodna
+            // 
+            this.btnPrethodna.Location = new System.Drawing.Point(293, 227);
+            this.btnPrethodna.Name = "btnPrethodna";
+            this.btnPrethodna.Size = new System.Drawing.Size(96, 23);
+            this.btnPrethodna.TabIndex = 16;
+            this.btnPrethodna.Text = "Prethodna";
+            this.btnPrethodna.UseVisualStyleBackColor = true;
+            // 
+            // btnSljedeca
+            // 
+            this.btnSljedeca.Location = new System.Drawing.Point(405, 227);
+            this.btnSljedeca.Name = "btnSljedeca";
+            this.btnSljedeca.Size = new System.Drawing.Size(96, 23);
+            this.btnSljedeca.TabIndex = 17;
+            this.btnSljedeca.Text = "Sljedeća";
+            this.btnSljedeca.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxOstecenja
+            // 
+            this.checkBoxOstecenja.AutoSize = true;
+            this.checkBoxOstecenja.Location = new System.Drawing.Point(91, 566);
+            this.checkBoxOstecenja.Name = "checkBoxOstecenja";
+            this.checkBoxOstecenja.Size = new System.Drawing.Size(90, 20);
+            this.checkBoxOstecenja.TabIndex = 19;
+            this.checkBoxOstecenja.Text = "Oštećenja";
+            this.checkBoxOstecenja.UseVisualStyleBackColor = true;
+            // 
+            // btnSlicni
+            // 
+            this.btnSlicni.Location = new System.Drawing.Point(107, 628);
+            this.btnSlicni.Name = "btnSlicni";
+            this.btnSlicni.Size = new System.Drawing.Size(118, 35);
+            this.btnSlicni.TabIndex = 20;
+            this.btnSlicni.Text = "Slični oglasi";
+            this.btnSlicni.UseVisualStyleBackColor = true;
+            // 
+            // btnZanimljivi
+            // 
+            this.btnZanimljivi.Location = new System.Drawing.Point(293, 628);
+            this.btnZanimljivi.Name = "btnZanimljivi";
+            this.btnZanimljivi.Size = new System.Drawing.Size(220, 35);
+            this.btnZanimljivi.TabIndex = 21;
+            this.btnZanimljivi.Text = "Dodaj u listu zanimljivih oglasa";
+            this.btnZanimljivi.UseVisualStyleBackColor = true;
+            // 
+            // btnOstecenja
+            // 
+            this.btnOstecenja.Location = new System.Drawing.Point(578, 628);
+            this.btnOstecenja.Name = "btnOstecenja";
+            this.btnOstecenja.Size = new System.Drawing.Size(118, 35);
+            this.btnOstecenja.TabIndex = 22;
+            this.btnOstecenja.Text = "Oštećenja";
+            this.btnOstecenja.UseVisualStyleBackColor = true;
+            // 
+            // btnKorime
+            // 
+            this.btnKorime.Location = new System.Drawing.Point(349, 769);
+            this.btnKorime.Name = "btnKorime";
+            this.btnKorime.Size = new System.Drawing.Size(96, 23);
+            this.btnKorime.TabIndex = 23;
+            this.btnKorime.Text = "Korime";
+            this.btnKorime.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(700, 842);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(96, 23);
+            this.btnClose.TabIndex = 24;
+            this.btnClose.Text = "Zatvori";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // FrmDetailAdAndAuctionReview
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 877);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnKorime);
+            this.Controls.Add(this.btnOstecenja);
+            this.Controls.Add(this.btnZanimljivi);
+            this.Controls.Add(this.btnSlicni);
+            this.Controls.Add(this.checkBoxOstecenja);
+            this.Controls.Add(this.btnSljedeca);
+            this.Controls.Add(this.btnPrethodna);
+            this.Controls.Add(this.pbSlika);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Marka);
+            this.Controls.Add(this.txtNazivOglasa);
+            this.Name = "FrmDetailAdAndAuctionReview";
+            this.Text = "FrmDetailAdAndAuctionReview";
+            ((System.ComponentModel.ISupportInitialize)(this.pbSlika)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNazivOglasa;
+        private System.Windows.Forms.Label Marka;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.PictureBox pbSlika;
+        private System.Windows.Forms.Button btnPrethodna;
+        private System.Windows.Forms.Button btnSljedeca;
+        private System.Windows.Forms.CheckBox checkBoxOstecenja;
+        private System.Windows.Forms.Button btnSlicni;
+        private System.Windows.Forms.Button btnZanimljivi;
+        private System.Windows.Forms.Button btnOstecenja;
+        private System.Windows.Forms.Button btnKorime;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
