2b04c243485b7205339f9ac3a68cd694be2a4f32
Andrej Antonić <aantonic20@foi.hr>
2023-01-02 23:49:59 +0100

    Kreirano učitavanje tekstualnih elemenata profila

--- Software/AutoPrime/Forms/FrmShowProfile.cs
+++ Software/AutoPrime/Forms/FrmShowProfile.cs
@@ -1,4 +1,5 @@
-﻿using EntitiesLayer.Entities;
+﻿using BusinessLogicModel.Services;
+using EntitiesLayer.Entities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,6 +15,7 @@ namespace AutoPrime
     public partial class FrmShowProfile : Form
     {
         Korisnik korisnik;
+        RecenzijaServices recenzijaService = new RecenzijaServices();
         public FrmShowProfile()
         {
             InitializeComponent();
@@ -32,7 +34,42 @@ namespace AutoPrime
 
         private void FrmShowProfile_Load(object sender, EventArgs e)
         {
+            LoadKorisnikData();
+        }
 
+        private void LoadKorisnikData()
+        {
+            txtUsername.Text = korisnik.Korimme;
+            txtPhoneNumber.Text = korisnik.Broj_telefona;
+            txtAverageRating.Text = GetAverageRating();
+            txtLastComment.Text = GetLastComment();
+        }
+
+        private string GetLastComment()
+        {
+            string comment;
+            List<Recenzija> recenzijas = recenzijaService.GetRecenzijas();
+            for(int i = 0; i < recenzijas.Count; i++)
+            {
+
+            }
+            return "temp";
+        }
+
+        private string GetAverageRating()
+        {
+            int averageRating = 0, numberOfRatings = 0;
+            List<Recenzija> recenzijas = recenzijaService.GetRecenzijas();
+            for(int i = 0; i < recenzijas.Count; i++)
+            {
+                if (recenzijas[i].Za_korisnik_id == korisnik.Id_korisnika)
+                {
+                    averageRating += recenzijas[i].Ocjena;
+                    numberOfRatings++;
+                }
+            }
+            averageRating /= numberOfRatings;
+            return averageRating.ToString();
         }
     }
 }
